5424a6da199b25489d63d9556e84bcb9b27e15bf
Joel Roske <joel.j.roske@gmail.com>
2020-09-23 18:13:22 -0500

    componentized articles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
-import ArticlesList from "./pages/ArticlesList";
+import ArticlesListPage from "./pages/ArticlesListPage";
 import NavBar from './NavBar';
 class App extends Component {
   render() {
@@ -15,7 +15,7 @@ class App extends Component {
           <div id="page-body">
           <Route exact path="/" component={HomePage} />
           <Route path="/about" component={AboutPage} />
-          <Route exact path="/articles-list" component={ArticlesList} />
+          <Route exact path="/articles-list" component={ArticlesListPage} />
           <Route path="/article/:name" component={ArticlePage} />
         </div>
         </div>
