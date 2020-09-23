b52a5f764d352999a31113620c98f0a756162c4a
Joel Roske <joel.j.roske@gmail.com>
2020-09-23 18:21:29 -0500

    Not Found Page logic set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
 import ArticlesListPage from "./pages/ArticlesListPage";
-import NavBar from './NavBar';
+import NotFoundPage from "./pages/NotFoundPage";
+import NavBar from "./NavBar";
 class App extends Component {
   render() {
     return (
@@ -13,11 +14,14 @@ class App extends Component {
         <div className="App">
           <NavBar />
           <div id="page-body">
-          <Route exact path="/" component={HomePage} />
-          <Route path="/about" component={AboutPage} />
-          <Route exact path="/articles-list" component={ArticlesListPage} />
-          <Route path="/article/:name" component={ArticlePage} />
-        </div>
+            <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/articles-list" component={ArticlesListPage} />
+            <Route path="/article/:name" component={ArticlePage} />
+            <Route component={NotFoundPage} />
+            </Switch>
+          </div>
         </div>
       </Router>
     );
